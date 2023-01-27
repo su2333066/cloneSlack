ad8cf1dc6ce1d49d47f10d92a9458cee31b8f84e
기수 <87399695+su2333066@users.noreply.github.com>
2023-01-27 12:21:18 +0900

    Add: back-end

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router';
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
     <Switch>
       <Redirect exact path="/" to="/login" />
       <Route path="/login" component={LogIn} />
-      <Route path="/login" component={SignUp} />
+      <Route path="/signup" component={SignUp} />
     </Switch>
   );
 };
