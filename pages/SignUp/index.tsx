c72e574925f76e11d58c6d66cc514bb2a9fd24e6
기수 <87399695+su2333066@users.noreply.github.com>
2023-02-01 17:47:25 +0900

    Make custom hook

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,20 +1,15 @@
+import useInput from '@hooks/useInput';
 import React, { useCallback, useState, VFC } from 'react';
 import { Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [signUpError, setSignUpError] = useState(false);
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+  const [mismatchError, setMismatchError] = useState(false);
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
-  const [mismatchError, setMismatchError] = useState(false);
-
-  const onChangeEmail = useCallback((e) => {
-    setEmail(e.target.value);
-  }, []);
-
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value);
-  }, []);
 
   const onChangePassword = useCallback(
     (e) => {
