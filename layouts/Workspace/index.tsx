8171514353b826326c96b1ecbd42d9b30de7a418
기수 <87399695+su2333066@users.noreply.github.com>
2023-02-07 01:04:08 +0900

    Make Menu

---
+++ layouts/Workspace/index.tsx
@@ -0,0 +1,87 @@
+import Menu from '@components/Menu';
+import React, { FC, useCallback, useState } from 'react';
+import loadable from '@loadable/component';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import gravatar from 'gravatar';
+import { Redirect, Route, Switch } from 'react-router';
+import {
+  Channels,
+  Chats,
+  Header,
+  LogOutButton,
+  MenuScroll,
+  ProfileImg,
+  ProfileModal,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
+
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
+
+const Workspace: FC = ({ children }) => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+  const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
+
+  const onLogout = useCallback(() => {
+    axios
+      .post('http://localhost:3095/api/users/logout', null, {
+        withCredentials: true,
+      })
+      .then(() => {
+        mutate(false, false);
+      });
+  }, []);
+
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
+  if (!data) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <Header>
+        <RightMenu>
+          <span onClick={onClickUserProfile}>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                <ProfileModal>
+                  <img src={gravatar.url(data.email, { s: '36px', d: 'retro' })} alt={data.nickname} />
+                  <div>
+                    <span id="profile-name">{data.nickname}</span>
+                    <span id="profile-active">Active</span>
+                  </div>
+                </ProfileModal>
+                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+              </Menu>
+            )}
+          </span>
+        </RightMenu>
+      </Header>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>
+          <Switch>
+            <Route path="/workspace/channel" component={Channel} />
+            <Route path="/workspace/dm" component={DirectMessage} />
+          </Switch>
+        </Chats>
+        {children}
+      </WorkspaceWrapper>
+    </div>
+  );
+};
+
+export default Workspace;
--- pages/Channel/index.tsx
+++ pages/Channel/index.tsx
@@ -1,11 +1,11 @@
-import Workspace from '@layouts/Workspace';
 import React from 'react';
+import { Container, Header } from './styles';
 
 const Channel = () => {
   return (
-    <Workspace>
-      <div>로그인 하신 것을 축하합니다.</div>
-    </Workspace>
+    <Container>
+      <Header>채널!</Header>
+    </Container>
   );
 };
 
